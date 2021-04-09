b7616377bd2cafa5ef7f67dabddeca6278621ad6
Jaeung Kim <jaeung949@gmail.com>
2021-04-09 09:44:51 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,16 @@
     <div class="home">
       <!-- <img alt="Vue logo" src="../assets/logo.png">
       <HelloWorld msg="여기는 지도자리"/> -->
-      <Map/>
     </div>
 </template>
 
 <script>
 // import HelloWorld from '@/components/HelloWorld.vue'
-import Map from '../components/Map.vue'
 
 export default {
   name: 'Home',
   components: {
     // HelloWorld,
-    Map,
   }
 }
 </script>
