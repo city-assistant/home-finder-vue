97aa6c60b99f6c83291c06c02fe704258875875b
Jaeung Kim <jaeung949@gmail.com>
2021-04-11 20:53:46 +0900

    get data from es with location

--- src/components/TownInfo.vue
+++ src/components/TownInfo.vue
@@ -8,6 +8,7 @@
 
 <script>
 import LineChart from './charts/LineChart'
+import axios from 'axios';
 
 export default {
     components: {
@@ -16,5 +17,72 @@ export default {
     props: {
         currentData: String,
     },
+    watch: {
+        currentData: function(val) {
+            axios.post('http://localhost:9200/officetel-rent-data/_search', {
+            "size": 0,
+            "sort": {
+                "@timestamp": {
+                "order": "asc"
+                }
+            },
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"전월세구분": "월세"}},
+                        {"prefix": {"시군구": {"value": "서울특별시 강서구 마곡동"}}}
+                    ]
+                } 
+            }, 
+            "aggs": {
+                "result": {
+                "date_histogram": {
+                    "field": "@timestamp",
+                    "calendar_interval": "year",
+                    "format": "yyyy"
+                },
+                "aggs": {
+                    "면적": {
+                    "avg": {
+                        "field": "area"
+                    }
+                    },
+                    "보증금": {
+                    "avg": {
+                        "field": "deposit"
+                    }
+                    },
+                    "월세": {
+                    "avg": {
+                        "field": "rent"
+                    }
+                    },
+                    "10평당 월세": {
+                    "bucket_script": {
+                        "buckets_path": {
+                        "v1": "면적",
+                        "v2": "월세"
+                        },
+                        "script": "params.v2 / params.v1 * 33"
+                    }
+                    },
+                    "10평당 보증금": {
+                    "bucket_script": {
+                        "buckets_path": {
+                        "v1": "면적",
+                        "v2": "보증금"
+                        },
+                        "script": "params.v2 / params.v1 * 33"
+                    }
+                    }
+                }
+                }
+            }
+            }).then(res => { 
+                console.log(res.data.aggregations.result.buckets)
+            })
+            console.log(val);
+        }
+    }
 }
 </script>
