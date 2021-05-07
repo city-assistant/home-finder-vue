b28bac9f9c6ea5e2d09cb33a2dcd96e4198e8451
ArkimCity <jaeung949@gmail.com>
2021-05-07 20:56:10 +0900

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,8 @@ import constant from './constant'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state:  {
+    // 여기서 접근한 아이피를 확인해서 로컬이 아닐 경우에는 BACK_SERVER_REAL 아닐 경우에는 BACK_SERVER_LOCAL 로 설정하는 기능을 추가하는 것이 좋겠다.
     SPRING_SERVER: constant.state.BACK_SERVER_REAL,
   },
   mutations: {
@@ -15,3 +16,4 @@ export default new Vuex.Store({
   modules: {
   }
 })
+
